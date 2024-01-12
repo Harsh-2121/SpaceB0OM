dbe878467f73a6a359c899ef8461370c95d35b4f
Harsh Pandey <141855181+Harsh-2121@users.noreply.github.com>
2024-01-12 06:55:04 -0800

    Add files via upload
    Game file

---
+++ Game.py
@@ -0,0 +1,112 @@
+print("SpaceB0OM")
+print("This text adventure is not for children, nor for adults who don't behave like children.")
+print("Author: Harsh Pandey")
+print("Copyright: Harsh Pandey Codings™")
+print("Space Craft")
+print("You are inside a space craft, possibly owned by very rich aliens. The craft is eerily silent, and you can see an open hatch to the east.")
+i1 = input(">")
+if i1 == "e" or i1 == "east":
+    print("Robot Room")
+    print("There are many robots here who are working on specific tasks. They all seem to be unaware of your presence. One robot in particular bangs you on the head quite hard and asks, 'Which planet do you come from?' The robot is wearing a red cap with 'T.R' written on it.")
+    i2 = input(">")
+    if i2 == "earth" or i2 == "Earth":
+        print("The robot examines you very carefully and moves aside to uncover an open secret door to the north.")
+        i3 = input(">")
+        if i3 == "n" or i3 == "north":
+            print("You go north and have a whirry, dizzy feeling and find yourself shut up in a lab.")
+            print("Electronics Lab")
+            print("This is a room with titanium walls all around it. There is a table with the following items: A soldering iron, a titanium drill bit, a drill motor, a drill chuck, some wood, and a hot glue gun.")
+            print("There are also some gauges on the wall. You can only read one: Oxygen. The gauge shows that it is quite low.")
+            i4 = input(">")
+            if i4 == "build drill" or i4 == "build project" or i4 == "make drill" or i4 == "build":
+                print("You quickly build a makeshift titanium drill.")
+                i5 = input(">")
+                if i5 == "drill wall" or i5 == "drill hole in wall" or i5 == "drill":
+                    print("The walls suddenly break apart and you get out.")
+                    print("Guarded Room")
+                    print("Two guards are approaching you. They don't look very friendly. You realize that the makeshift drill you made has turned into a 12000346200000000000000000000 megavolt laser.")
+                    print("There is an open door behind the guards to the north.")
+                    i6 = input(">")
+                    if i6 == "kill guards" or i6 == "kill guards with laser":
+                        print("The guards try to kill you first, but by very, very bad luck for them, they miss. Your trusty 12000346200000000000000000000 megavolt laser does not miss and kills them instantly.")
+                        print("The door seems to be clear.")
+                        i7 = input(">")
+                        if i7 == "n" or i7 == "north":
+                            print("You move forward and enter the...")
+                            print("Spacecraft Room")
+                            print("There is a small spacecraft here with a sticker that says 'WARNING!This spacecraft can only hold one earth person.'.")
+                            i8 = input(">")
+                            if i8 == "enter spacecraft" or i8 == "in":
+                              print("You get in the space craft and the motion detection triggers.The spacecraft launches and reaches...")
+                              print("Planet BH 1234")
+                              print("You fall through the seemingly atmosphereless sky and crash land in the basement of The Space Invaders Computer University(SICU).There is a laptop here with the label of Space and Time Bender and Teleporter.The air is suprisingly breathable.")
+                              print("There is a opening to the south.")
+                              i9 = input(">")
+                              if i9 =="s" or i9 == "south":
+                                print("You get out of the dusty old building and find a very small kitten meowing in pain on a brick.")
+                                i10 = input(">")
+                                if i10 =="help kitten":
+                                  print("You do what you can,and the kitten finally recovers.It meows in thanks and leaps away.There is a rocket here with the hatch open.")
+                                  i11 = input(">")
+                                  if i11 =="in" or i11 == "get in rocket":
+                                    print("The rocket's hatch seals itself and launches.After a few hours(or days),the rocket lands and you get out to discover...")
+                                    print("Planet XFDR")
+                                    print("There is a console here that says 'Hurry! Type in the correct password to save the planet!!!!!'.")
+                                    i12 = input("Password:")
+                                    if i12 == "robot":
+                                      print("The planet opens up and and you find The Gem, the most powerful object in the universe, only fit for a daring interstellar explorer.")
+                                      print("The power of the Gem is unlocked by you, and the Gem replies:'Daring explorer, you have ventured far and wide for instructions and it is this- Go find and kill Ravager, the interstellar monster.'")
+                                      i13 = input(">")
+                                      if i13 == "get in rocket" or i13 == "in":
+                                        print("The rocket, realising you work for the Gem and are now one of the elite Order of the Gem,immediately sends you to Black Hole 2000, where Ravager currently resides.")
+                                        print("Ravager immediately tries to attack.What the hell do you do?")
+                                        i14 = input(">")
+                                        if i14 == "activate lasers" or i14 == "laser":
+                                          print("Bam!Kapow!! Oh lord he is coming back for another attack!!")
+                                          i15 = input(">")
+                                          if i15 == "engage missiles" or i15 == "missiles":
+                                            print("BOOOOOOOOOOOOOOOOOOOOOOOOM!!!!!!!!!!!!!!!!!!!!!!!")
+                                            print("The rocket is thrown back with the explosion of a dying Ravager.Whew!")
+                                            print("The rocket is now heading back to Planet XFDR.")
+                                            print("You land.")
+                                            print("Congrats, you just won SpaceB00M!!!!")
+                                          else:
+                                            print("Oh my googily moogily.Its best not to tell you what happened.")
+                                            print("****You died very stoopidly****")
+                                        else:
+                                          print("The Gem is very angry now, and sends you to the 13th Dimension.You are now trapped with a bunch of murderers.Needless to say, you die instantly.")
+                                          print("****You died very stoopidly****")
+                                    else:
+                                      print("You gave the wrong password and the planet exploded.")
+                                      print("****You died very stoopidly****")
+                                  else:
+                                    print("The rocket reaveals itself to be a nuke, and explodes in your face.Naturally, you die.")
+                                    print("****You died very stoopidly****")
+                                else:
+                                  print("The small kitten dies and the Lord Of Justice roars 'You did not help the needy and let them die.YOU WILL DIEEEEEEEEEEEE!' and immediately kills you with the mighty scale of justice.")
+                                  print("****You died very stoopidly****")
+                              else:
+                                print("The laptop,you discover,bends time and reality within seconds.You explode in a very entertaining laser explosion.")
+                                print("****You died very stoopidly****")
+                            else:
+                              print("The guards come to life and run through the open door and kill you.")
+                              print("****You died very stoopidly****")
+                        else:
+                            print("The guards come to life and kill you for sweet, sweet revenge.")
+                            print("****You died very stoopidly****")
+                    else:
+                        print("The guards immediately kill you and you die.")
+                        print("****You died very stoopidly****")
+                else:
+                    print("The Oxygen level runs out and you die.")
+                    print("****You died very stoopidly****")
+            else:
+                print("You bang your head against the wall in an effort to break it and pant a lot. This results in the oxygen level dropping to 0% and you die.")
+                print("****You died very stoopidly****")
+        else:
+            print("You promptly crash into a robot with a glowing cap and letters 'H.V' written on it. You now realize that the robot runs on 120000 volts! You are immediately killed.")
+            print("****You died very stoopidly****")
+    else:
+        print("The robot burns you to death by using an infrared laser because you told a lie and truth robots just don't accept that, like mothers.")
+else:
+    print("Not valid!")
